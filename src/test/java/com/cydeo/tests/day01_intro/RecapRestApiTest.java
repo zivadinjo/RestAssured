c328cfd9f72a146c8894468addda0d2e5a16e9ef
zivadinjo <zivadinjo1985@gmail.com>
2022-07-31 15:40:44 -0500

    day01

--- src/test/java/com/cydeo/tests/day01_intro/RecapRestApiTest.java
+++ src/test/java/com/cydeo/tests/day01_intro/RecapRestApiTest.java
@@ -4,13 +4,14 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
 import static io.restassured.RestAssured.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * When User sends GET Request to
  * https://reqres.in/api/users
- *
+ * <p>
  * Then Response status code should be 200
  * And Response body should contain "George"
  * And Header Content type should be json
@@ -26,11 +27,11 @@ public class RecapRestApiTest {
 
         //WHEN USER SENDS GET REQUEST
         //Response response = RestAssured.get(url); --> without static import
-        Response response= when().get(url); // with already imported restAssured, and when() is making it more readable (gherkin syntax)
+        Response response = when().get(url); // with already imported restAssured, and when() is making it more readable (gherkin syntax)
 
         //Then response status should be 200
-        System.out.println("Status code ="+response.statusCode());
-        assertEquals(200,response.statusCode());
+        System.out.println("Status code =" + response.statusCode());
+        assertEquals(200, response.statusCode());
 
         //BDD syntax
         response.then().statusCode(200);//assertion we can add assertThat() after than()
@@ -39,16 +40,47 @@ public class RecapRestApiTest {
         response.prettyPrint();
 
         //And response body should contain "George"
-        System.out.println("Response json body = "+ response.asString());
-        assertTrue(response.asString().contains("George"),"Verification failed");
+        System.out.println("Response json body = " + response.asString());
+        assertTrue(response.asString().contains("George"), "Verification failed");
 
         //And Header type should be json
-        System.out.println("Content type header value = "+ response.contentType());
-        assertTrue(response.contentType().contains("application/json"),"Verification failed");
+        System.out.println("Content type header value = " + response.contentType());
+        assertTrue(response.contentType().contains("application/json"), "Verification failed");
 
 
     }
 
+    /**
+     * When User Sends get request to API Endpoint:
+     * "https://reqres.in/api/users/5"
+     * Then Response status code should be 200
+     * And Response body should contain user info "Charles"
+     */
+
+
+    @Test
+    public void getSingleUserApiTest() {
+
+    Response response= when().get(url+"/5");
+
+        //Then response status should be 200
+        System.out.println("Status code =" + response.statusCode());
+        assertEquals(200, response.statusCode());
+
+
+        //print response body in a formatted way
+        response.prettyPrint();
+
+        //And response body should contain "George"
+        System.out.println("Response json body = " + response.asString());
+        assertTrue(response.asString().contains("Charles"), "Verification failed");
+
+        //And Header type should be json
+        System.out.println("Content type header value = " + response.contentType());
+        assertTrue(response.contentType().contains("application/json"), "Verification failed");
+
+
+    }
 
 
 }
