a11a64c2c0e159f27f95a4ce6e72c0a1e2cea5b5
zivadinjo <zivadinjo1985@gmail.com>
2022-08-06 11:54:02 -0500

    day04

--- src/test/java/com/cydeo/tests/day04_jsonpath/HRApiGetTest.java
+++ src/test/java/com/cydeo/tests/day04_jsonpath/HRApiGetTest.java
@@ -5,6 +5,7 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 
 import io.restassured.http.ContentType;
+import io.restassured.internal.path.json.JSONAssertion;
 import io.restassured.response.Response;
 import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.BeforeEach;
@@ -45,5 +46,57 @@ public class HRApiGetTest {
         assertTrue(response.body().asString().contains("Europe"),"Verification of body failed");
 
     }
+
+    /**
+     * Given accept type is json
+     And Path param "region_id" value is 1
+     When user send get request to /ords/hr/regions/{region_id}
+     Status code should be 200
+     Content type should be "application/json"
+     And body should contain "Europe"
+     */
+    @Test
+    public void getSinglePathParamName(){
+
+        Response response=given().log().all().accept(ContentType.JSON).
+                and().pathParam("region_id",1).when().get("/regions/{region_id}");
+        response.prettyPrint();
+
+        assertEquals(200,response.statusCode(),"Verification of status code failed");
+        //assertEquals(HttpStatus.SC_OK,response.statusCode(),"Verification of status code failed");
+        assertEquals("application/json",response.contentType(),"Verification of content failed");
+        //assertEquals(ContentType.JSON.toString(),response.contentType(),"Verification of content failed");
+        assertTrue(response.body().asString().contains("Europe"),"Verification of body failed");
+
+    }
+
+    /**
+     * Given accept type is json
+     * And query param q={"region_name": "Americas"}
+     * When user send get request to /ords/hr/regions
+     * Status code should be 200
+     * Content type should be "application/json"
+     * And region name should be "Americas"
+     * And region id should be "2"
+     */
+
+    @Test
+    public void getRegionWithQueryParamTest(){
+
+        Response response=given().log().all().accept(ContentType.JSON).
+                and().queryParam("q","{\"region_name\": \"Americas\"}")
+                .when().get("/regions");
+
+        response.prettyPrint();
+
+        assertEquals(200,response.statusCode(),"Verification of status code failed");
+        //assertEquals(HttpStatus.SC_OK,response.statusCode(),"Verification of status code failed");
+        assertEquals("application/json",response.contentType(),"Verification of content failed");
+        //assertEquals(ContentType.JSON.toString(),response.contentType(),"Verification of content failed");
+        assertTrue(response.body().asString().contains("Americas"),"Verification of body failed");
+        assertTrue(response.body().asString().contains("\"region_id\": 2"), "Verification of region id failed");
+
+    }
+
 
 }
