f035eb0f1d1f0699675cddd34b4b6b5f46025496
zivadinjo <zivadinjo1985@gmail.com>
2022-08-07 11:08:48 -0500

    day04

---
+++ src/test/java/com/cydeo/utils/HRApiTestBase.java
@@ -0,0 +1,14 @@
+package com.cydeo.utils;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+
+
+public class HRApiTestBase {
+
+    @BeforeAll //@BeforeClass in junit 4
+    public static void setUp() {
+        RestAssured.baseURI = ConfigurationReader.getProperty("hr.api.url");
+    }
+
+}
