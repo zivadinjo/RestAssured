8fd5053872ef1a89960bfaaf6e93d88f362d5686
zivadinjo <zivadinjo1985@gmail.com>
2022-08-13 13:33:31 -0500

    day08

--- src/test/java/com/cydeo/tests/day08_hamcrest/SpartanHamcrestTest.java
+++ src/test/java/com/cydeo/tests/day08_hamcrest/SpartanHamcrestTest.java
@@ -6,6 +6,8 @@ import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
@@ -37,6 +39,43 @@ public class SpartanHamcrestTest extends SpartanTestBase {
                         "name",is("Julio"),
                         "gender",is("Male"),
                         "phone",is(9393139934L));
+
+
+    }
+
+    /**
+     * Given accept type is json
+     *      And query param nameContains value is "e"
+     *      And query param gender value is "Female"
+     *      When I send get request to /spartans/search
+     *      Then status code is 200
+     *      And content type is Json
+     *      And header date contains 2022
+     *      And json response data is as expected
+     *         totalElement is 34
+     *         has ids: 94, 98,91, 81
+     *         has names: Jocelin, Georgianne, Catie, Marylee,Elita
+     *         every gender is Female
+     *         every name contains e
+     */
+    @DisplayName("GET/spartans/search -> hamcrest assertion")
+    @Test
+    public void searchTest(){
+
+        given().accept(ContentType.JSON)
+                .and().queryParam("nameContains","e")
+                .and().queryParam("gender","Female")
+                .when().get("spartans/search")
+                .then().statusCode(200)
+                .and().contentType(ContentType.JSON)
+                .and().header("Date",containsString("2022"))
+                .and().header("Date",containsString(LocalDate.now().getYear()+""))
+                .and().body("totalElement",is(equalTo(34)),
+                        "content.id",hasItems(94, 98,91, 81),
+                        "content.name",hasItems("Jocelin", "Georgianne", "Catie", "Marylee","Elita"),
+                        "content.gender",everyItem(is("Female")),
+                        "content.name",everyItem(containsStringIgnoringCase("e")))
+                .log().all();//log all response details
 
 
     }
