d9af229224af02b8d763e4a20e9aff76e5168f42
zivadinjo <zivadinjo1985@gmail.com>
2022-08-17 20:36:23 -0500

    day11

--- src/test/java/com/cydeo/utils/SpartanRestUtils.java
+++ src/test/java/com/cydeo/utils/SpartanRestUtils.java
@@ -2,6 +2,10 @@ package com.cydeo.utils;
 
 import com.cydeo.pojo.Spartan;
 import com.github.javafaker.Faker;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 
@@ -39,4 +43,15 @@ public class SpartanRestUtils {
         return spartan;
     }
 
+    //Method accept spartanId and sends GET request,
+    //and return is Map object containing json data
+    public static Map<String, Object> getSpartan(int spartanId) {
+        Response response = given().accept(ContentType.JSON)
+                .and().pathParam("id", spartanId)
+                .when().get(baseUrl + "/spartans/{id}");
+
+        Map<String, Object> spartanMap = response.as(Map.class);
+        return spartanMap;
+
+    }
 }
