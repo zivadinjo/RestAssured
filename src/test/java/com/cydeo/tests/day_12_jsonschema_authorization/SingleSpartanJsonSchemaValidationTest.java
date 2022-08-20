3b1cb7e5c31d018bded66fbdc00ae1f255f8d930
zivadinjo <zivadinjo1985@gmail.com>
2022-08-20 11:05:42 -0500

    day12

---
+++ src/test/java/com/cydeo/tests/day_12_jsonschema_authorization/SingleSpartanJsonSchemaValidationTest.java
@@ -0,0 +1,39 @@
+package com.cydeo.tests.day_12_jsonschema_authorization;
+
+import com.cydeo.utils.SpartanTestBase;
+import io.restassured.module.jsv.JsonSchemaValidator;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import io.restassured.http.ContentType;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class SingleSpartanJsonSchemaValidationTest extends SpartanTestBase {
+
+    /**
+     * given accept type is json
+     * and path param id is 15
+     * when I send GEt request to /spartans/{id}
+     * then status code is 200
+     * And json payload/body matches SingleSpartanSchema.json
+     */
+
+    @Test
+    public void singleSpartanSchema(){
+
+        given().accept(ContentType.JSON)
+                .and().pathParam("id",15)
+                .when().get("spartans/{id}")
+                .then().statusCode(200)
+                .and().body(JsonSchemaValidator.matchesJsonSchema(new File("src/test/resources/jsonschemas/SingleSpartanSchema.json")))
+                .and().log().all();
+
+    }
+}
