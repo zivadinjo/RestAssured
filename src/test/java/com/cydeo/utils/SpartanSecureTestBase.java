bed1e380a35aa9945da2a4dae8df0dbcfcf5f8cb
zivadinjo <zivadinjo1985@gmail.com>
2022-08-23 19:40:05 -0500

    day14

--- src/test/java/com/cydeo/utils/SpartanSecureTestBase.java
+++ src/test/java/com/cydeo/utils/SpartanSecureTestBase.java
@@ -1,14 +1,23 @@
 package com.cydeo.utils;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.BeforeAll;
 
+import static io.restassured.RestAssured.given;
+
 public abstract class SpartanSecureTestBase {
+
+    protected static RequestSpecification requestSpec;
 
     @BeforeAll
     public static void setUp(){
 
         RestAssured.baseURI= ConfigurationReader.getProperty("spartan.secure.api.url");
+
+        requestSpec = given().accept(ContentType.JSON)
+                .and().auth().basic("admin", "admin");
 
     }
 
