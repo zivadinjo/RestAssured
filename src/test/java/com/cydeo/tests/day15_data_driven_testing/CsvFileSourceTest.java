cb558a8205c9182e7d9f77866c635c6fb31ea31e
zivadinjo <zivadinjo1985@gmail.com>
2022-08-24 20:48:31 -0500

    day15

---
+++ src/test/java/com/cydeo/tests/day15_data_driven_testing/CsvFileSourceTest.java
@@ -0,0 +1,40 @@
+package com.cydeo.tests.day15_data_driven_testing;
+
+import com.cydeo.utils.ConfigurationReader;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static io.restassured.RestAssured.*;
+
+public class CsvFileSourceTest {
+    @BeforeAll
+    public static void setUp() {
+        baseURI = ConfigurationReader.getProperty("zip.code.api.url");
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/ZipCodes.csv", numLinesToSkip = 1)
+    public void zipCodeTest(String state, String city, int zipCodeCount){
+
+        Map<String, String> paramsMap = new HashMap<>();
+        paramsMap.put("state",state);
+        paramsMap.put("city",city);
+
+        given().accept(ContentType.JSON)
+                .and().pathParams(paramsMap)
+                .when().get("/us/{state}/{city}")
+                .then().assertThat().statusCode(200)
+                .and().contentType(ContentType.JSON)
+                .and().body("places",hasSize(zipCodeCount))
+                .log().all();
+
+    }
+}
