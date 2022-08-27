10b94b2896bb58063c5e678b947b0cc51cf2a4ff
zivadinjo <zivadinjo1985@gmail.com>
2022-08-27 10:58:57 -0500

    day16

---
+++ src/test/java/com/cydeo/tests/day16_methodsource_mocks/MethodSourceTest.java
@@ -0,0 +1,35 @@
+package com.cydeo.tests.day16_methodsource_mocks;
+
+import com.cydeo.utils.ConfigurationReader;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.*;
+
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static io.restassured.RestAssured.*;
+
+public class MethodSourceTest {
+    /**
+     *Data provider method
+     */
+
+    public static List<String> getCountries(){
+        List<String> countries = Arrays.asList("Canada","USA","France","Turkey","Mexico","Egypt","Germany");
+
+        return countries;
+    }
+
+    @ParameterizedTest
+    @MethodSource("getCountries")
+    public void countriesTest(String countryName){
+        System.out.println("countryName = " + countryName);
+        System.out.println(countryName.length());
+    }
+}
